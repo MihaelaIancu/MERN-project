6ac29957f5b2a1011146b510c4bd000afac94514
MihaelaIancu <iancumihaela46@yahoo.com>
2022-07-26 22:15:06 +0300

    routes & mynotes comp

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,12 +4,15 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">Note Zipper</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
@@ -23,7 +26,9 @@ function Header() {
             </Form>
           </Nav>
           <Nav>
-            <Nav.Link href="#home">My Notes</Nav.Link>
+            <Nav.Link>
+              <Link to="/mynotes">My Notes</Link>
+            </Nav.Link>
             <NavDropdown title="Mihaela" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
               <NavDropdown.Divider />
---
+++ frontend/src/screens/MyNotes/MyNotes.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import MainScreen from "../../components/MainScreen";
+import { Link } from "react-router-dom";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import notes from "../../data/notes";
+import "./MyNotes.css";
+
+const MyNotes = () => {
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+
+  return (
+    <MainScreen title="Welcome Back user...">
+      <Link to="createnote">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+      {notes.map((note, index) => (
+        <Accordion flush key={index}>
+          <Accordion.Item eventKey="0">
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Accordion.Header as={Card.Text} variant="link">
+                    {note.title}
+                  </Accordion.Header>
+                </span>
+                <div>
+                  <Button href={`/note/${note._id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Body>
+                <Card.Body>
+                  <h4>
+                    <Badge bg="success">Category - {note.category}</Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Created on - date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Body>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </MainScreen>
+  );
+};
+
+export default MyNotes;
